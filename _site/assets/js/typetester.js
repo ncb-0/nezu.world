4ae69d9e7e232dbf9a99b1cd54e33aa199bde69f
ncb0 <frogheck@gmail.com>
2022-04-27 18:09:01 -0400

    type tester randomized sample text

--- _site/assets/js/typetester.js
+++ _site/assets/js/typetester.js
@@ -38,6 +38,26 @@
 function typeTester() {
   let testers = document.querySelectorAll(".testercontainer");
 
+  // sample text randomizer
+  let sampleStrings = [
+    "Even if you say everything she feels is “simulated”, remember that for Yumi it’s REAL pain…",
+    "Yes, I’m in a private place where I can speak freely… Her mood? Umm… More meds? Wha—",
+    "REMEMBER WE HAD A RULE, YANG?! THE “NO MILITARY ANDROIDS IN MY HOUSE” ONE?!",
+    "Hilariously, the pins are colour-coded by chakra. She’s the only robot I’ve seen with all seven.",
+    "Your voice is unique! It reminds me of a game I played, with cute little singing turrets…",
+    "(The Emotion Engine was reconnected, causing a rush of new emotions to flow through her!) “WOW!!!”",
+    "He thinks of robots as simple machines, to be tooled with and reprogrammed as you see fit.",
+    "“So what’d he string you up in? Drugs? Auto theft? Gambling?” “The second one, Feefs.”",
+    "“The more I look at it—it—it resembles my proportions so closely?!” “Haha, the final version did have skin on top, so…”",
+    "If the voice is right… then I’m not like you… or anyone else… and… and… UGH!!! IT’S WRONG!!!",
+    "I can feel it… they’re stealing all our data as we speak… and right now they’re rummaging through my bunny pics.",
+    "I’m just a crazy person. I don’t know what I’m doing. So why are we doing things my way…"
+  ];
+
+  let shuffledStrings = sampleStrings.sort(() => 0.5 - Math.random());
+
+  console.log(shuffledStrings);
+
   for (let i = 0; i < testers.length; i++) {
     let testerText         = testers[i].querySelector(".tester");
 
@@ -60,6 +80,9 @@ function typeTester() {
       let fontWeight = fontWeightSelector.value;
       testerText.style.fontWeight = fontWeight;
     }
+
+    // testerText.innerHTML = sampleStrings[Math.floor(Math.random() * sampleStrings.length)];
+    testerText.innerHTML = shuffledStrings[i];
   }
 }
 
